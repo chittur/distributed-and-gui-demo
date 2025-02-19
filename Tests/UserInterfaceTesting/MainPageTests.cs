dfffcb18c4a0fc97716df4df7e789d200a90ffbd
Ramaswamy Krishnan Chittur <chittur@gmail.com>
2025-02-19 20:04:02 +0530

    Add end to end tests for image send/receive flow. AB#7

--- Tests/UserInterfaceTesting/MainPageTests.cs
+++ Tests/UserInterfaceTesting/MainPageTests.cs
@@ -10,6 +10,7 @@
  * Description = User interface tests for the main page.
  *****************************************************************************/
 
+using System.Drawing;
 using FlaUI.Core;
 using FlaUI.Core.AutomationElements;
 using FlaUI.UIA3;
@@ -97,5 +98,100 @@ public class MainPageTests
         TextBox receiveMessageTextBox1 = window1.FindFirstDescendant(cf => cf.ByAutomationId("ReceiveMessageTextBox")).AsTextBox();
         Logger.LogMessage($"Window1 ReceiveMessageTextBox: {receiveMessageTextBox1.Text}");
         Assert.AreEqual(Message, receiveMessageTextBox1.Text);
+    }
+
+    /// <summary>
+    /// Tests that images can be sent and received between two instances of the application.
+    /// </summary>
+    /// <returns>Task</returns>
+    [TestMethod]
+    [Owner("Ramaswamy Krishnan-Chittur")]
+    public async Task TestSendAndReceiveImageViaUI()
+    {
+        // Arrange
+
+        // Find the receive port of the first instance of the application.
+        Window window1 = _app1.GetMainWindow(_automation);
+        TextBox receivePortTextBox1 = window1.FindFirstDescendant(cf => cf.ByAutomationId("ReceivePortTextBox")).AsTextBox();
+        int port1 = int.Parse(receivePortTextBox1.Text);
+        Logger.LogMessage($"Window1 ReceivePortTextBox: {port1}");
+
+        // Find the receive port of the second instance of the application.
+        Window window2 = _app2.GetMainWindow(_automation);
+        TextBox receivePortTextBox2 = window2.FindFirstDescendant(cf => cf.ByAutomationId("ReceivePortTextBox")).AsTextBox();
+        int port2 = int.Parse(receivePortTextBox2.Text);
+        Logger.LogMessage($"Window2 ReceivePortTextBox: {port2}");
+
+        // Act
+
+        // From the second instance of the application, enter an image file and the destination
+        // port of the first application, and click the Send Image button.
+        const string ImageFilename = @"Resources\TestImageFile.jpg";
+        TextBox imageFileTextBox2 = window2.FindFirstDescendant(cf => cf.ByAutomationId("SendImageTextBox")).AsTextBox();
+        imageFileTextBox2.Enter(ImageFilename);
+        TextBox sendPortTextBox2 = window2.FindFirstDescendant(cf => cf.ByAutomationId("SendPortTextBox")).AsTextBox();
+        sendPortTextBox2.Enter($"{port1}");
+        Button sendImageButton2 = window2.FindFirstDescendant(cf => cf.ByAutomationId("SendImageButton")).AsButton();
+        sendImageButton2.Invoke();
+        Logger.LogMessage($"Window2 SendPortTextBox:{port1}, SendImageTextBox: {ImageFilename}");
+
+        // Wait for a short while for the image to be received.
+        Logger.LogMessage("Waiting for a short while for the image to be received");
+        await Task.Delay(1000);
+
+        // Assert
+
+        // Check if the image was received by the first instance of the application.
+        AutomationElement imageControl = window1.FindFirstDescendant(cf => cf.ByControlType(FlaUI.Core.Definitions.ControlType.Image));
+        Bitmap image1 = imageControl.Capture();
+        // resize the original image to the size of the image control, and compare to the received image.
+        Bitmap orignal = new Bitmap(ImageFilename);
+        Bitmap resized = ResizeBitmap(orignal, image1.Width, image1.Height);
+        CompareImages(resized, image1);
+    }
+
+    /// <summary>
+    /// Resizes a bitmap to the specified width and height.
+    /// </summary>
+    /// <param name="bitmap">Given bitmap</param>
+    /// <param name="width">Width to resize to</param>
+    /// <param name="height">Height to resize to</param>
+    /// <returns>Resized image.</returns>
+    private static Bitmap ResizeBitmap(Bitmap bitmap, int width, int height)
+    {
+        Bitmap resizedBitmap = new Bitmap(width, height);
+        using (Graphics g = Graphics.FromImage(resizedBitmap))
+        {
+            g.DrawImage(bitmap, 0, 0, width, height);
+        }
+
+        return resizedBitmap;
+    }
+
+    /// <summary>
+    /// Compares two bitmaps for equality.
+    /// </summary>
+    /// <param name="bitmap1">First image</param>
+    /// <param name="bitmap2">Second image</param>
+    /// <returns>A value indicating whether the images are similar.</returns>
+    private static bool CompareImages(Bitmap bitmap1, Bitmap bitmap2)
+    {
+        if (bitmap1.Width != bitmap2.Width || bitmap1.Height != bitmap2.Height)
+        {
+            return false;
+        }
+
+        for (int y = 0; y < bitmap1.Height; y++)
+        {
+            for (int x = 0; x < bitmap1.Width; x++)
+            {
+                if (bitmap1.GetPixel(x, y) != bitmap2.GetPixel(x, y))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
